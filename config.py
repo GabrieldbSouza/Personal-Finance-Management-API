29629bfc05b0887fd357232b18eb3dd3a7eb04a0
Gabriel <gabrieldebritosouza15@gmail.com>
2024-03-30 00:53:35 -0300

    Adcionando config para as rotas home e user

--- config.py
+++ config.py
@@ -1,9 +1,13 @@
+from routes.home import home_route
+from routes.user import user_route
 
 def config_all(app):
-  pass
+  config_routes(app)
 
 def config_routes(app):
-  pass
+  app.register_blueprint(home_route)
+  app.register_blueprint(user_route, url_prefix = '/user/<int:user_id>/')
+
 
 def config_db(app):
   pass
